652b5bc2573a0b7a35467f4d1238e630a332f9c2
elGOATdelcode <keantatum84@proton.me>
2025-09-10 22:23:02 -0500

    primer commit

---
+++ Unidad 1/Actividad 1/programa_saludo.py
@@ -0,0 +1,10 @@
+#Ejercicios de la actividad 1
+
+# a. Programa que muestre: "Hola, este es mi primer programa en python"
+print("Hola, este es mi primer programa en python.")
+
+# b. Programa que solicite nombre/edad y muestre: "Hola [nombre], tienes [edad] años"
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Hola", nombre, "tienes", edad, "años")
